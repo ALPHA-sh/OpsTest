cd2d20b05b6b1fdcd91f32b66a7dd526eb8ef9dc
aklassen21 <40897901+ALPHA-sh@users.noreply.github.com>
2018-10-01 08:33:35 -0400

    :b::b::b::b::b::b::b::b:

---
+++ src/OperationsPractice.java
@@ -0,0 +1,21 @@
+
+public class OperationsPractice {
+
+	public static void main(String[] args) 
+	{
+		int var1;
+		var1 = 100;
+		System.out.println("original: " + var1 );
+		var1 += 5;
+		System.out.println("+5 = " + var1 );
+		var1 /= 2;
+		System.out.println("/2 = " +  var1 );
+		var1 *= 4;
+		System.out.println("*4 = " + var1 );
+		var1 -= 6;
+		System.out.println("-6 = " + var1 );
+		var1 %= 2;
+		System.out.println("%2 = " + var1 + " (end result)");
+	}
+
+}
